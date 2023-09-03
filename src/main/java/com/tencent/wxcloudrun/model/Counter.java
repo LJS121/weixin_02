ac1001416564c6b3c970a30087c43865978d1fd9
LinJiaRui.霖嘉睿 <linjiarui@pinduoduo.com>
2023-09-04 00:05:10 +0800

    add

---
+++ src/main/java/com/tencent/wxcloudrun/model/Counter.java
@@ -0,0 +1,18 @@
+package com.tencent.wxcloudrun.model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
+public class Counter implements Serializable {
+
+  private Integer id;
+
+  private Integer count;
+
+  private LocalDateTime createdAt;
+
+  private LocalDateTime updatedAt;
+}
---
+++ src/main/java/com/tencent/wxcloudrun/service/CounterService.java
@@ -0,0 +1,14 @@
+package com.tencent.wxcloudrun.service;
+
+import java.util.Optional;
+
+import com.tencent.wxcloudrun.model.Counter;
+
+public interface CounterService {
+
+  Optional<Counter> getCounter(Integer id);
+
+  void upsertCount(Counter counter);
+
+  void clearCount(Integer id);
+}
