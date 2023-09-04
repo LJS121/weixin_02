ef621f25ea54e164186953d5ca5a5b8ae149213d
LinJiaRui.霖嘉睿 <linjiarui@pinduoduo.com>
2023-09-04 11:42:12 +0800

    add

--- src/main/java/com/tencent/wxcloudrun/controller/FansController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/FansController.java
@@ -4,8 +4,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.alibaba.fastjson.JSON;
-
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -19,7 +17,7 @@ public class FansController {
 
     @PostMapping("/fans/msg")
     public String fansMsgPush(String msg) {
-        log.info("fans_push_msg={}", JSON.toJSONString(msg));
+        log.info("fans_push_msg={}", msg);
         return "<xml>\n"
             + "  <ToUserName><![CDATA[toUser]]></ToUserName>\n"
             + "  <FromUserName><![CDATA[fromUser]]></FromUserName>\n"
